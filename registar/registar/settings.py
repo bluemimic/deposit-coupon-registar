d2e4e89ee913b150006eb35801f6106bd47011ee
bluemimic <75360541+bluemimic@users.noreply.github.com>
2026-03-29 20:08:15 +0300

    Refactor database settings to use SQLite and remove MySQL configuration; update Docker Compose to streamline services

--- registar/registar/settings.py
+++ registar/registar/settings.py
@@ -91,12 +91,8 @@ WSGI_APPLICATION = 'registar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('MYSQL_DATABASE'),
-        'USER': os.getenv('MYSQL_USER'),
-        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'db'), 
-        'PORT': os.getenv('DB_PORT', '3306'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
